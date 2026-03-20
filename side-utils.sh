419279ece2cc9d31c4f586e945ffa8d1d6a3357a
albrrak773 <albrrak773@gmail.com>
2026-03-20 18:06:09 +0300

    replace discord with whatsapp and open discord as an app

--- side-utils.sh
+++ side-utils.sh
@@ -4,6 +4,7 @@
 
 notify-send "Launching Workspace" "Side Utils" -t 2000
 
-uwsm-app -- google-chrome-stable --new-window "https://discord.com/app" "http://localhost:8096" &
+uwsm-app -- discord &
+uwsm-app -- google-chrome-stable --new-window "http://localhost:8096" "https://web.whatsapp.com" &
 omarchy-launch-webapp "https://www.notion.so/The-Event-Loop-2f98fdd5c8d780ed849ce68206994be6" &
 uwsm-app -- spotify &
